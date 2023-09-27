286d97dde215fb01a976735588b0e8a743baf6f8
fededomm <fededomm@outlook.it>
2023-09-27 18:04:19 +0200

    add appconfig dockerfile docker compose

---
+++ app-config.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+
+	util "book-store-be/utils"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Log           LogOptions `yaml:"log" mapstructure:"log" json:"log"`
+	Database      DbOptions  `yaml:"database" mapstructure:"database" json:"database"`
+	Observability ObsOptions `yaml:"observability" mapstructure:"observability" json:"observability"`
+}
+type DbOptions struct {
+	ConnectionString string `yaml:"connectionString" mapstructure:"connectionString" json:"connectionString"`
+}
+
+type ObsOptions struct {
+	ServiceName string `yaml:"serviceName" mapstructure:"serviceName" json:"serviceName"`
+	Endpoint    string `yaml:"endpoint" mapstructure:"endpoint" json:"endpoint"`
+	Enable      bool   `yaml:"enable" mapstructure:"enable" json:"enable"`
+}
+type LogOptions struct {
+	Level      int  `yaml:"level" mapstructure:"level" json:"level"`
+	EnableJSON bool `yaml:"enableJson" mapstructure:"enableJson" json:"enableJson"`
+}
+
+// Default Config il file is not found
+var DefaultConfig = Config{
+	Log: LogOptions{
+		Level:      -1,
+		EnableJSON: false,
+	},
+	Database: DbOptions{
+		ConnectionString: "",
+	},
+	Observability: ObsOptions{
+		ServiceName: "",
+		Endpoint:    "",
+		Enable:      false,
+	},
+}
+
+// Default config file.
+//
+//go:embed config.yaml
+var projectConfigFile []byte
+
+const ConfigFileEnvVar = "BOOK_STORE_BE_FILE_PATH"
+const ConfigurationName = "BOOK_STORE_BE"
+
+/*
+???    thanks to Mario Imperato for the snippets!!!
+*/
+func ReadConfig() (*Config, error) {
+
+	configPath := os.Getenv(ConfigFileEnvVar)
+	var cfgContent []byte
+	var err error
+	if configPath != "" {
+		if _, err := os.Stat(configPath); err == nil {
+			log.Info().Str("cfg-file-name", configPath).Msg("reading config")
+			cfgContent, err = util.ReadFileAndResolveEnvVars(configPath)
+			log.Info().Msg("++++CFG:" + string(cfgContent))
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			return nil, fmt.Errorf("the %s env variable has been set but no file cannot be found at %s", ConfigFileEnvVar, configPath)
+		}
+	} else {
+		log.Warn().Msgf("The config path variable %s has not been set. Reverting to bundled configuration", ConfigFileEnvVar)
+		cfgContent = util.ResolveConfigValueToByteArray(projectConfigFile)
+		// return nil, fmt.Errorf("the config path variable %s has not been set; please set", ConfigFileEnvVar)
+	}
+
+	appCfg := DefaultConfig
+	err = yaml.Unmarshal(cfgContent, &appCfg)
+	if err != nil {
+		log.Fatal().Err(err).Send()
+	}
+
+	if !appCfg.Log.EnableJSON {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	}
+
+	zerolog.SetGlobalLevel(zerolog.Level(appCfg.Log.Level))
+
+	return &appCfg, nil
+}
--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module book-store-be
 
 go 1.21.0
 
+require github.com/rs/zerolog v1.31.0
+
 require (
 	github.com/bytedance/sonic v1.10.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
@@ -18,6 +20,7 @@ require (
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
