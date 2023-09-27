cf77245519e64abf8694b16183472ace911b7b7d
Christopher Deboli <chrisd3v3l0p@gmail.com>
2023-09-27 17:22:24 +0200

    Adds new properties in Book struct

--- models/book_model.go
+++ models/book_model.go
@@ -1,12 +1,13 @@
 package models
 
 type Book struct {
-	Titolo          string  `json:"titolo" bson:"titolo" validate:"required"`
-	Autore          string  `json:"autore" bson:"autore" validate:"required"`
-	Prezzo          float32 `json:"prezzo" bson:"prezzo" validate:"required"`
-	Summary         string  `json:"summary" bson:"summary" validate:"required"`
-	CopertinaRigida bool    `json:"copertina_rigida" bson:"copertina_rigida" validate:"required"`
-	Genere          string  `json:"genere" bson:"genere" validate:"required"`
-	Quantita        int     `json:"quantita" bson:"quantita" validate:"required"`
-	IdFoto          string  `json:"id_foto" bson:"id_foto" validate:"required"`
+	Titolo      string  `json:"titolo" bson:"titolo" validate:"required,max=255"`
+	Autore      string  `json:"autore" bson:"autore" validate:"required"`
+	Prezzo      float32 `json:"prezzo" bson:"prezzo" validate:"required"`
+	Summary     string  `json:"summary" bson:"summary" validate:"required,max=512"`
+	Copertina   string  `json:"copertina" bson:"copertina" validate:"required,eq=Hard Cover|eq=Flexible Cover"`
+	Genere      string  `json:"genere" bson:"genere" validate:"required,eq=Novel|eq=Fantasy|eq=Business|eq=Psychology|eq=Design|eq=Fiction"`
+	Quantita    int     `json:"quantita" bson:"quantita" validate:"required,gte=1,lte=5"`
+	Categoria   string  `json:"categoria"  bson:"categoria" validate:"required,eq=Best Seller|eq=New Releases|eq=Best Offers"`
+	IdCopertina string  `json:"id_copertina" bson:"id_copertina" validate:"required"`
 }
