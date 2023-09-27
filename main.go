61175c07e484642805350315d217cc6153be6077
Christopher Deboli <chrisd3v3l0p@gmail.com>
2023-09-27 12:38:45 +0200

    Adds gin-gonic packages

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
-import "book-store-be/database"
+import (
+	"book-store-be/database"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	// connect to the databasa
 	database.ConnectDatabase()
+
+	// gin
+	r := gin.Default()
+	r.Run()
 }
