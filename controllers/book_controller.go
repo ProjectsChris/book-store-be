e43a05cd20c54e00701a1e286e6cf8ffc8c51510
Christopher Deboli <chrisd3v3l0p@gmail.com>
2023-09-27 16:21:58 +0200

    Removes auto increment and GetBooks function

--- controllers/book_controller.go
+++ controllers/book_controller.go
@@ -9,13 +9,12 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson"
+	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var bookCollection *mongo.Collection = database.ConnectDatabase().Database("BOOK-STORE").Collection("Books")
-var counterCollection *mongo.Collection = database.ConnectDatabase().Database("BOOK-STORE").Collection("Counters")
+var validate *validator.Validate = validator.New()
 
 // PostBook function add new book
 func PostBook(c *gin.Context) {
@@ -30,75 +29,19 @@ func PostBook(c *gin.Context) {
 		return
 	}
 
-	// auto increment id
-	var auto_increment map[string]any = bson.M{}
-	err := counterCollection.FindOneAndUpdate(
-		ctx,
-		bson.D{{
-			Key:   "_id",
-			Value: "bookId",
-		}},
-		bson.D{{
-			Key: "$inc",
-			Value: bson.D{{
-				Key:   "seq",
-				Value: 1,
-			}},
-		}},
-		options.FindOneAndUpdate().SetReturnDocument(options.After).SetUpsert(true),
-	).Decode(&auto_increment)
-	if err != nil {
-		responses.ResponseMessage(c, http.StatusInternalServerError, "error", err.Error())
+	// check validator
+	validationError := validate.Struct(&book)
+	if validationError != nil {
+		responses.ResponseMessage(c, http.StatusInternalServerError, "error", validationError.Error())
 		return
 	}
 
-	// change value of the id
-	book.Id = auto_increment["seq"].(int32)
-
 	// adds new book
-	_, err = bookCollection.InsertOne(ctx, book)
+	_, err := bookCollection.InsertOne(ctx, book)
 	if err != nil {
 		responses.ResponseMessage(c, http.StatusInternalServerError, "error", err.Error())
 		return
 	}
 
 	responses.ResponseMessage(c, http.StatusOK, "success", "added new book")
-}
-
-// GetBooks return a JSON with a list of books
-func GetBooks(c *gin.Context) {
-	// create a deadline
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	// find all books
-	cur, err := bookCollection.Find(ctx, bson.D{})
-	if err != nil {
-		responses.ResponseMessage(c, http.StatusInternalServerError, "error", err.Error())
-		return
-	}
-	defer cur.Close(ctx)
-
-	// create an array
-	var books []models.Book
-
-	// execute all content of cursor
-	for cur.Next(ctx) {
-		var b models.Book
-
-		err := cur.Decode(&b)
-		if err != nil {
-			responses.ResponseMessage(c, http.StatusInternalServerError, "error", err.Error())
-			return
-		}
-
-		// add new book inside an array
-		books = append(books, b)
-	}
-	if err = cur.Err(); err != nil {
-		responses.ResponseMessage(c, http.StatusInternalServerError, "error", err.Error())
-		return
-	}
-
-	responses.ResponseMessage(c, http.StatusOK, "success", books)
 }
--- models/book_model.go
+++ models/book_model.go
@@ -1,12 +1,12 @@
 package models
 
 type Book struct {
-	Id              int32   `json:"id" bson:"id"`
-	Titolo          string  `json:"titolo" bson:"titolo"`
-	Autore          string  `json:"autore" bson:"autore"`
-	Prezzo          float32 `json:"prezzo" bson:"prezzo"`
-	Summary         string  `json:"summary" bson:"summary"`
-	CopertinaRigida bool    `json:"copertina_rigida" bson:"copertina_rigida"`
-	Genere          string  `json:"genere" bson:"genere"`
-	Quantita        int     `json:"quantita" bson:"quantita"`
+	Titolo          string  `json:"titolo" bson:"titolo" validate:"required"`
+	Autore          string  `json:"autore" bson:"autore" validate:"required"`
+	Prezzo          float32 `json:"prezzo" bson:"prezzo" validate:"required"`
+	Summary         string  `json:"summary" bson:"summary" validate:"required"`
+	CopertinaRigida bool    `json:"copertina_rigida" bson:"copertina_rigida" validate:"required"`
+	Genere          string  `json:"genere" bson:"genere" validate:"required"`
+	Quantita        int     `json:"quantita" bson:"quantita" validate:"required"`
+	IdFoto          string  `json:"id_foto" bson:"id_foto" validate:"required"`
 }
